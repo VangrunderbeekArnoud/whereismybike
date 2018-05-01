8338ec24d2fa9f9bded7d0249f8967718614e5dd
Arnoud Vangrunderbeek <arnoud.vangrunderbeek@gmail.com>
2018-05-01 12:54:18 +0200

    app module imports cleanup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,6 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { IonicStorageModule } from '@ionic/storage';
 import { StatusBar } from '@ionic-native/status-bar';
 import { Camera } from '@ionic-native/camera';
-import { ImagePicker } from '@ionic-native/image-picker';
-import { BackgroundMode } from '@ionic-native/background-mode';
-import { CardIO } from '@ionic-native/card-io';
-import { Stripe } from '@ionic-native/stripe';
-import { DatePicker } from '@ionic-native/date-picker';
-import { LocalNotifications } from '@ionic-native/local-notifications';
-import { HttpModule } from '@angular/http';
-import { Facebook } from '@ionic-native/facebook';
 import { Vibration } from '@ionic-native/vibration';
 import { BrowserTab} from '@ionic-native/browser-tab';
 import { Firebase} from '@ionic-native/firebase';
@@ -22,8 +14,6 @@ import * as firebase from 'firebase/app'
 import { OneSignal} from '@ionic-native/onesignal';
 import { InAppBrowser } from '@ionic-native/in-app-browser';
 import { CallNumber } from '@ionic-native/call-number';
-import { Ionic2RatingModule } from 'ionic2-rating';
-import { PayPal, PayPalPayment, PayPalConfiguration } from '@ionic-native/paypal';
 import { TranslateModule} from 'ng2-translate/ng2-translate';
 import { TranslateLoader, TranslateStaticLoader} from 'ng2-translate/src/translate.service';
 import { Http} from '@angular/http';
@@ -64,10 +54,8 @@ firebase.initializeApp(firebaseConfig);
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
-    Ionic2RatingModule,
     IonicStorageModule.forRoot(),
     BrowserModule,
-    HttpModule,
     TranslateModule.forRoot({
       provide: TranslateLoader,
       useFactory: (createTranslateLoader),
@@ -84,23 +72,15 @@ firebase.initializeApp(firebaseConfig);
     {provide: ErrorHandler, useClass: IonicErrorHandler},
     AuthProvider,
     ProfileProvider,
-    CardIO,
     BrowserTab,
-    ImagePicker,
     Camera,
-    Facebook,
     GeocoderProvider,
     PopUpProvider,
     OnesignalProvider,
-    BackgroundMode,
     OneSignal,
-    DatePicker,
-    LocalNotifications,
     InAppBrowser,
     CallNumber,
     Firebase,
-    Stripe,
-    PayPal,
     NativeMapContainerProvider,
     GoogleMaps,
     Vibration,
